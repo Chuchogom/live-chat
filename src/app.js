0df8bdd494d55115e4e1fe1d2ad914505ec54083
jesusgomrec <jesus.gomrec@gmail.com>
2023-07-08 12:48:53 -0600

    Live chat with Websockets

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import __dirname from './utils.js'
+import { Server } from 'socket.io'
+import handlebars from 'express-handlebars'
+import routerViews from './router/views.router.js'
+
+const app = express()
+const httpServer = app.listen(8080, () => console.log("Listenning on port..."))
+const io = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+app.use('/static', express.static(__dirname + '/public'))
+
+
+app.get('/health', (req, res) => res.send('Ok'))
+app.use('/', routerViews)
+
+const messages = []
+io.on('connection', socket => {
+    socket.on('new', user => console.log(`${user} is connected`))
+
+    socket.on('message', data => {
+        messages.push(data)
+        io.emit('logs', messages)
+    })
+})
